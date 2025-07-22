0c1eca27be002dec2c361246b3e3bed3e100eeb9
UmangPansuriya30 <umangpansuriya@weybee.in>
2025-07-22 19:08:57 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using HtmlAgilityPack;
+using System.Net.Http;
+
+class Program
+{
+    static String URL = "https://ineichen.com/auctions/past/";
+    public static void Main(string[] args)
+    {
+        var htmlClient = new HttpClient();
+        var html = htmlClient.GetStringAsync(URL).Result;
+        var htmlDocument = new HtmlDocument();
+        htmlDocument.LoadHtml(html);
+
+        string filePath = "C:/WorkingDirectory/Tasks/WebScraping/index.html";
+        //File.WriteAllText(filePath, htmlDocument.ParsedText);
+
+        var auctionItemList = htmlDocument.DocumentNode.SelectNodes("//div[@class='auctions-list']//div[@class='auction-item']");
+
+        File.WriteAllText(filePath,auctionItemList.ElementAt(0).InnerHtml);
+
+    }
+}
