fe04dead4a6b8d7d32273bae31ddda7e03cf567a
UmangPansuriya30 <umangpansuriya@weybee.in>
2025-07-23 14:47:33 +0530

    Updated

---
+++ AuctionDB.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Data.SqlClient;
+
+
+namespace WebScraping
+{
+    class AuctionDB
+    {
+        private static string CONNECTION_STRING = "Data Source=DESKTOP-T625HOC\\SQLEXPRESS;Initial Catalog=WebScraping;Integrated Security=True;Trust Server Certificate=True";
+        
+        public static int insertIntoAuction(Auction auction)
+        {
+            SqlConnection conn = new SqlConnection(CONNECTION_STRING);
+            conn.Open();
+            SqlCommand cmd = new SqlCommand()
+            {
+                Connection = conn,
+                CommandType = System.Data.CommandType.StoredProcedure,
+                CommandText = "sp_InsertAuction",
+                Parameters =
+                {
+                    new SqlParameter("Title",auction.Title),
+                    new SqlParameter("Description",auction.Description),
+                    new SqlParameter("ImageUrl",auction.ImageUrl),
+                    new SqlParameter("Link",auction.Link),
+                    new SqlParameter("LotCount",auction.LotCount),
+                    new SqlParameter("StartDate",auction.StartDate),
+                    new SqlParameter("StartMonth",auction.StartMonth),
+                    new SqlParameter("StartYear",auction.StartYear),
+                    new SqlParameter("StartTime",auction.StartTime),
+                    new SqlParameter("EndDate",auction.EndDate),
+                    new SqlParameter("EndMonth",auction.EndMonth),
+                    new SqlParameter("EndYear",auction.EndYear),
+                    new SqlParameter("EndTime",auction.EndTime),
+                    new SqlParameter("Location",auction.Location),
+                }
+            };
+            int affectedRow = cmd.ExecuteNonQuery();
+            conn.Close();
+            return affectedRow;
+
+        }
+        public static void truncateAuction()
+        {
+            SqlConnection conn = new SqlConnection(CONNECTION_STRING);
+            conn.Open();
+            SqlCommand cmd = new SqlCommand()
+            {
+                Connection = conn,
+                CommandType = System.Data.CommandType.StoredProcedure,
+                CommandText = "sp_TruncateAuction"
+            };
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,22 +1,107 @@
 ﻿using HtmlAgilityPack;
+using System.Globalization;
 using System.Net.Http;
+using System.Text.RegularExpressions;
+using WebScraping;
 
 class Program
 {
-    static String URL = "https://ineichen.com/auctions/past/";
+    static string URL = "https://ineichen.com/auctions/past/";
+
+    public static HtmlNode getElementByXPath(string XPath, HtmlNode htmlNode)
+    {
+        return htmlNode.SelectSingleNode(XPath);
+    }
+
+    public static string descriptionFormatter(string description)
+    {
+        return Regex.Replace(description.Trim(), @"[ \t]*\n[ \t]*(\n[ \t]*)*", "\n");
+    }
+
+    public static string regexMatcher(string input, string pattern)
+    {
+        Match match = Regex.Match(input, pattern);
+        if (match.Success)
+            return match.Value.Trim();
+        else
+            return "0";
+    }
+
     public static void Main(string[] args)
     {
         var htmlClient = new HttpClient();
         var html = htmlClient.GetStringAsync(URL).Result;
         var htmlDocument = new HtmlDocument();
         htmlDocument.LoadHtml(html);
-
-        string filePath = "C:/WorkingDirectory/Tasks/WebScraping/index.html";
-        //File.WriteAllText(filePath, htmlDocument.ParsedText);
+        AuctionDB.truncateAuction();
 
         var auctionItemList = htmlDocument.DocumentNode.SelectNodes("//div[@class='auctions-list']//div[@class='auction-item']");
 
-        File.WriteAllText(filePath,auctionItemList.ElementAt(0).InnerHtml);
+        Auction auction = new Auction();
+        foreach (var item in auctionItemList)
+        {
+            auction = new Auction();
+            string className = "auction-item__";
 
+            #region ImageURL,Title Extraction
+            auction.ImageUrl = getElementByXPath($".//a[@class='{className}image']//img[@src]", item).Attributes["src"]?.Value;
+            auction.Title = getElementByXPath($".//div[@class='{className}title']//h2[@class='{className}name']//a[@href]", item).InnerText.Trim();
+            #endregion
+
+            #region Button Link and LotCount Extraction
+            HtmlNode viewBtn = getElementByXPath($".//div[@class='{className}btns']//a[@href]", item);
+            auction.Link = viewBtn.Attributes["href"]?.Value;
+            auction.LotCount = int.Parse(regexMatcher(viewBtn.InnerText, @"\d+"));
+            #endregion
+
+            className = "auction-date-location";
+
+            #region Description,Location Extraction
+            auction.Description = descriptionFormatter(getElementByXPath($".//div[@class='{className}']", item).InnerText);
+            auction.Location = regexMatcher(auction.Description, @"\n.*$");
+            #endregion
+
+            string dataTime = regexMatcher(auction.Description, @"^.*\r?\n");
+
+            #region Date Extraction
+            MatchCollection dateRegex = Regex.Matches(dataTime, @"\b\d{1,2}\b");
+            if (dateRegex.Count() == 1)
+                auction.StartDate = auction.EndDate = int.Parse(dateRegex.ElementAt(0).Value);
+            else if (dateRegex.Count() == 2)
+            {
+                auction.StartDate = int.Parse(dateRegex.ElementAt(0).Value);
+                auction.EndDate = int.Parse(dateRegex.ElementAt(1).Value);
+            }
+            #endregion
+
+            #region year Extracted
+            auction.StartYear = auction.EndYear = int.Parse(regexMatcher(dataTime, @"\d{4}"));
+            #endregion
+
+            #region Month Extraction
+            MatchCollection monthRegex = Regex.Matches(dataTime, @"[a-zA-z]+");
+            if (monthRegex.Count() == 1)
+                auction.StartMonth = auction.EndMonth = monthRegex.ElementAt(0).Value;
+            else if (monthRegex.Count() == 2)
+            {
+                auction.StartMonth = monthRegex.ElementAt(0).Value;
+                auction.EndMonth = monthRegex.ElementAt(1).Value;
+            }
+            #endregion
+
+            #region Time Extraction
+            MatchCollection timeRegex = Regex.Matches(auction.Description, @"\d{1,2}:\d{1,2}");
+            if (timeRegex.Count() == 1)
+                auction.StartTime = TimeSpan.Parse(timeRegex.ElementAt(0).Value);
+            else if (timeRegex.Count() == 2)
+            {
+                auction.StartTime = TimeSpan.Parse(timeRegex.ElementAt(0).Value);
+                auction.EndTime = TimeSpan.Parse(timeRegex.ElementAt(1).Value);
+            }
+            #endregion
+
+            AuctionDB.insertIntoAuction(auction);
+        }
+        Console.WriteLine("Crawed Successfully");
     }
 }
